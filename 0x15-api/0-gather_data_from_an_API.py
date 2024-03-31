51a10e28c1a307bb0f3a11d50fb0fa07afabdd92
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-03-31 13:30:44 -0700

    correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,48 @@
 #!/usr/bin/python3
-"""_summary_"""
+"""
+Python script that, using this REST API, for a given employee ID
+returns information about his/her TODO list progress
+use urllib or requests module
+accepts an integer as a parameter, which is the employee ID
+"""
+
+
+import json
+import requests
+from sys import argv
+
+
+def emp_information():
+    """
+    returns information about employee TODO list progress
+    """
+    TASK_TITLE = []
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+
+    emp_id = int(argv[1])
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    for user in users.json():
+        if user.get('id') == emp_id:
+            EMPLOYEE_NAME = user.get('name')
+            break
+
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for task in tasks.json():
+        if task.get('userId') == emp_id:
+            TOTAL_NUMBER_OF_TASKS += 1
+            if task.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUMBER_OF_TASKS
+                                                          ))
+
+    for title in TASK_TITLE:
+        print("\t {}".format(title))
 
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
-
-    id = int(argv[1])
-    users = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    tasks = requests.get("https://jsonplaceholder.typicode.com/users").json()
-    task  = 0
-    total = 0
-
-    for value in users:
-        if value['userId'] == id:
-            total += 1
-            if value['completed']:
-                task += 1
-
-    for user in tasks:
-        if user['id'] == id:
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(user['name'], task, total))
-            for value in users:
-                if value['userId'] == id and value['completed']:
-                    print("\t {}".format(value['title']))
+    emp_information()
