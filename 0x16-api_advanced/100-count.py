2e185155d2e77743d66d26746f0ae89a67c60517
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-04-10 08:24:02 -0700

    count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
 """
-recursive function that queries the Reddit API
+queries the Reddit API
 """
-
 import requests
-from collections import defaultdict
 
-def count_words(subreddit, word_list, after=None, counts=defaultdict(int)):
-    """
-    connect to the reddit API
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}  # Custom User-Agent to avoid Too Many Requests error
-    params = {'limit': 100, 'after': after}  # Limiting the number of posts to retrieve to 100 per request
 
-    response = requests.get(url, headers=headers, params=params)
-    
-    # Check if the request was successful
+def count_words(subreddit, word_list, after=None, counts=None):
+    """
+    recursive function that queries the Reddit API, parses the title
+    """
+
+    if counts is None:
+        counts = {}
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    else:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
-        
-        # Extract titles and count occurrences of keywords
+
         for post in posts:
             title = post['data']['title'].lower()
             for word in word_list:
                 if f" {word.lower()} " in f" {title} ":
-                    counts[word.lower()] += 1
-        
-        # Check if there are more posts to fetch
+                    counts[word.lower()] = counts.get(word.lower(), 0) + 1
         after = data['data']['after']
         if after is not None:
-            # Recursively call the function with the 'after' parameter to fetch the next page of posts
             return count_words(subreddit, word_list, after, counts)
         else:
-            # No more posts to fetch, print the sorted counts
             sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
             for word, count in sorted_counts:
                 print(f"{word}: {count}")
     elif response.status_code == 404:
-        print("Invalid subreddit or no posts found.")
+        print("nonexisting subreddit")
     else:
-        print("An error occurred while fetching data from Reddit.")
-        
-
+        print("existing Subreddit")
