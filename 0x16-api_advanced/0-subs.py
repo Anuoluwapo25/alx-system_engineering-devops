3d86ff0bf10a01778d280cbed6061f5fbf5aba15
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-04-10 07:45:35 -0700

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,29 @@
-#!/usr/bin/python3
-"""
-queries the Reddit API
-returns the number of subscribers
-If invalid subreddit is given, returns 0
-"""
-
-
 import requests
 from sys import argv
 
-
 def number_of_subscribers(subreddit):
     """
-    connect to reddit API
-    returns the number of subscribers
+    Returns the number of subscribers for a given subreddit.
     """
-    subreddit = argv[1]
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    url = f"https://api.reddit.com/r/{subreddit}/about"
 
-    urlRequest = requests.get(url, headers=headers)
-    if urlRequest.status_code == 200:
-        response = urlRequest.json()
-        numSubscribers = response["data"]["subscribers"]
+    url_request = requests.get(url, headers=headers)
+    if url_request.status_code == 200:
+        response = url_request.json()
+        subs = response["data"]["subscribers"]
+        return subs
+    elif url_request.status_code == 404:
+        return "nonexisting subreddit"
     else:
-        numSubscribers = 0
-    return numSubscribers
+        return "existing Subreddit"
+
+# Call the function with the subreddit provided as a command-line argument
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python script.py subreddit_name")
+    else:
+        subreddit_name = argv[1]
+        result = number_of_subscribers(subreddit_name)
+        print(result)
+
